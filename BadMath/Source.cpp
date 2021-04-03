3cb6dabe118e5d367b880d01b567691ff1c273f5
Chase Morris <mr.maol23@outlook.com>
2021-04-03 19:05:30 -0400

    Fixed File

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -1,26 +1,36 @@
+// Chase Morris 
+// CIS 1202  
+// 4/3/2021
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+// calculates the average of the three numbers. 
+float average(float *i1, float *i2, float *i3) // fixed integrial incoming values and passed by reference 
 {
-	return (i1 + i2 + i3) / 3; 
+	return (*i1 + *i2 + *i3) / 3; 
 }
 
+// asks the user for the three numbers to be averaged, the calls the average function and displays 
+// the average of said numbers. 
 int main()
 {
-	int n1,n2,n3;
+	float n1,n2,n3; // set the numbers as float, you also do not need to initialize them because they
+	// will be initialized 
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n3;
+	cout << "Third number: "; cin >> n3; // fixed over write of n2 
 
-	float a = average(n1, n2, n3);
+	float a = average(&n1, &n2, &n3); // passed in variables by reference, do not use up space copying 
+	// values into functions 
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(5) << fixed << "The average is " << a << endl; // fixed setprecision to 1, you may want
+	// a more precise reading than just .x, .xxxxx is better 
 
+	// fixed program auto closes 
 	cin.get(); 
 	cin.ignore();
 
-	return 0;
+	// you do not need to return 0 at the end of main, as it already does that 
 }
